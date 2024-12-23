608bd46d4adc6663db2e420950ea0435aee90dff
David Longo <dave@davidjlongo.com>
2024-12-22 20:05:32 -0500

    kill all external

--- scripts/kill_external.sh
+++ scripts/kill_external.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 perl -i -p0e 's/requests\.post\(.*?(?=\))\)/requests\.post\(\"\#\"\,data\=None\)/s' src/boltz/data/msa/mmseqs2.py
+perl -i -p0e 's/https\:\/\/api\.colabfold\.com/\#/s' src/boltz/data/msa/mmseqs2.py
+perl -i -p0e 's/https\:\/\/api\.colabfold\.com/\#/s' src/boltz/main.py
